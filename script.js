5f72f2b8e0d9a54da0136c8930aa5e10d1b0d028
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-08-29 10:57:10 +0200

    Add font family to exported diagram CSS

--- script.js
+++ script.js
@@ -1136,6 +1136,7 @@ const diagramCssLight = `
 .conn.red{fill:#d33;}
 .conn.blue{fill:#369;}
 .conn.green{fill:#090;}
+text{font-family:'Open Sans',sans-serif;}
 .edge-label{font-size:10px;}
 line{stroke:#333;stroke-width:2px;}
 path.edge-path{stroke:#333;stroke-width:2px;fill:none;}
@@ -1148,7 +1149,7 @@ const diagramCssDark = `
 .node-box{fill:#333;stroke:none;}
 .node-box.first-fiz{stroke:none;}
 .first-fiz-highlight{stroke:url(#firstFizGrad);}
-text{fill:#fff;}
+text{fill:#fff;font-family:'Open Sans',sans-serif;}
 line{stroke:#fff;}
 path.edge-path{stroke:#fff;}
 path.power{stroke:#ff6666;}
