51eb14f1d766b5e6e48bee34db895c01ff803a1f
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-08-30 20:20:27 +0200

    Handle missing lz-string dependency in tests

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   testEnvironment: 'jsdom',
   setupFiles: ['jest-localstorage-mock'],
+  moduleNameMapper: {
+    '^lz-string$': '<rootDir>/tests/__mocks__/lz-string.js'
+  }
 };
