d50295ac28d56c607937c0a92e092911a5bb4079
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-08-30 21:44:55 +0200

    Add basic PWA support with service worker

---
+++ service-worker.js
@@ -0,0 +1,35 @@
+/* eslint-env serviceworker */
+const CACHE_NAME = 'camera-power-planner-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/data.js',
+  '/translations.js',
+  '/storage.js',
+  '/normalizeData.js',
+  '/unifyPorts.js',
+  '/favicon.ico',
+  '/manifest.webmanifest'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key)))
+    )
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => resp || fetch(event.request))
+  );
+});
