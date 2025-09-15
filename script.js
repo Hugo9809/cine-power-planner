0b8a82f102b77e16af5af3bd4c4b8ceea1d24e7b
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-09-15 17:04:33 +0200

    fix: persist large monitor selections

--- script.js
+++ script.js
@@ -9826,7 +9826,11 @@ function getCurrentGearListHtml() {
                 const originalSel = gearListOutput.querySelector(`#gearList${role}Monitor`);
                 const val = originalSel ? originalSel.value : sel.value;
                 Array.from(sel.options).forEach(opt => {
-                    opt.selected = (opt.value === val);
+                    if (opt.value === val) {
+                        opt.setAttribute('selected', '');
+                    } else {
+                        opt.removeAttribute('selected');
+                    }
                 });
             }
         });
@@ -9836,7 +9840,11 @@ function getCurrentGearListHtml() {
                 const originalSel = gearListOutput.querySelector(`#gearList${role}Monitor15`);
                 const val = originalSel ? originalSel.value : sel.value;
                 Array.from(sel.options).forEach(opt => {
-                    opt.selected = (opt.value === val);
+                    if (opt.value === val) {
+                        opt.setAttribute('selected', '');
+                    } else {
+                        opt.removeAttribute('selected');
+                    }
                 });
             }
         });
