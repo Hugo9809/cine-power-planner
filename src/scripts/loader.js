5488c480d417ecee9a2a50055bf3e4dbef22d619
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-10-06 01:44:56 +0200

    Expose auto gear module API globally

--- src/scripts/loader.js
+++ src/scripts/loader.js
@@ -1083,6 +1083,7 @@ CRITICAL_GLOBAL_DEFINITIONS.push({
     'src/scripts/modules/globals.js',
     'src/scripts/modules/offline.js',
     'src/scripts/modules/core-shared.js',
+    'src/scripts/modules/features/auto-gear-rules.js',
     'src/scripts/modules/features/backup.js',
     'src/scripts/modules/ui.js',
     'src/scripts/app-core-new-1.js',
