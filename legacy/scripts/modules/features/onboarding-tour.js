4ac06b38fcb21336d654bb4c07793b994588b957
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-10-12 17:05:29 +0200

    Align Power Summary onboarding highlights

--- legacy/scripts/modules/features/onboarding-tour.js
+++ legacy/scripts/modules/features/onboarding-tour.js
@@ -1495,16 +1495,16 @@ function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == 
       }
     }, {
       key: 'resultsTotalDraw',
-      highlight: '#results'
+      highlight: ['#powerDiagram', '#totalPowerLabel', '#totalPower', '#totalCurrent144Label', '#totalCurrent12Label']
     }, {
       key: 'resultsBatteryPacks',
-      highlight: '#results'
+      highlight: ['#batteryLifeLabel', '#batteryLife', '#batteryCountLabel', '#batteryCount']
     }, {
       key: 'resultsChangeover',
-      highlight: '#results'
+      highlight: '#breakdownList'
     }, {
       key: 'resultsWarnings',
-      highlight: '#results'
+      highlight: ['#pinWarning', '#dtapWarning', '#hotswapWarning']
     }, {
       key: 'batteryComparison',
       highlight: '#batteryComparison'
