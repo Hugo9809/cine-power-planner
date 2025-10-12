3f4dbc810b3a99b1474ac19015e2cc74f7d16fad
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-10-13 00:52:13 +0200

    Refactor runtime state module into modular components

---
+++ legacy/scripts/modules/core/runtime-state/safe-freeze-registry.js
@@ -0,0 +1,161 @@
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+(function () {
+  function detectAmbientScope() {
+    if (typeof globalThis !== 'undefined' && globalThis && _typeof(globalThis) === 'object') {
+      return globalThis;
+    }
+
+    if (typeof window !== 'undefined' && window && _typeof(window) === 'object') {
+      return window;
+    }
+
+    if (typeof self !== 'undefined' && self && _typeof(self) === 'object') {
+      return self;
+    }
+
+    if (typeof global !== 'undefined' && global && _typeof(global) === 'object') {
+      return global;
+    }
+
+    return null;
+  }
+
+  function fallbackHasArrayEntry(array, value) {
+    if (!Array.isArray(array)) {
+      return false;
+    }
+
+    for (var index = 0; index < array.length; index += 1) {
+      if (array[index] === value) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  function resolveScopeUtils() {
+    var scopeUtils = null;
+
+    if (typeof require === 'function') {
+      try {
+        scopeUtils = require('./scope-utils.js');
+      } catch (scopeUtilsRequireError) {
+        void scopeUtilsRequireError;
+      }
+    }
+
+    if (scopeUtils) {
+      return scopeUtils;
+    }
+
+    var scope = detectAmbientScope();
+    if (scope && _typeof(scope) === 'object') {
+      try {
+        var registry = scope.cineCoreRuntimeStateModules;
+        if (registry && _typeof(registry) === 'object' && registry.scopeUtils) {
+          return registry.scopeUtils;
+        }
+      } catch (scopeLookupError) {
+        void scopeLookupError;
+      }
+    }
+
+    return null;
+  }
+
+  var scopeUtils = resolveScopeUtils();
+  var hasArrayEntry = scopeUtils && typeof scopeUtils.hasArrayEntry === 'function' ? scopeUtils.hasArrayEntry : fallbackHasArrayEntry;
+
+  function registerSafeFreezeEntry(registry, value) {
+    if (!registry || !value) {
+      return registry;
+    }
+
+    if (typeof registry.add === 'function') {
+      try {
+        registry.add(value);
+      } catch (registryAddError) {
+        void registryAddError;
+      }
+      return registry;
+    }
+
+    if (!hasArrayEntry(registry, value) && Array.isArray(registry)) {
+      registry.push(value);
+    }
+
+    return registry;
+  }
+
+  function createSafeFreezeRegistry(initialValues) {
+    var registry = typeof WeakSet === 'function' ? new WeakSet() : [];
+
+    if (Array.isArray(initialValues)) {
+      for (var index = 0; index < initialValues.length; index += 1) {
+        try {
+          registerSafeFreezeEntry(registry, initialValues[index]);
+        } catch (initialisationError) {
+          void initialisationError;
+        }
+      }
+    }
+
+    return registry;
+  }
+
+  function ensureSafeFreezeRegistry(registry, initialValues) {
+    if (registry && (typeof registry.add === 'function' || Array.isArray(registry))) {
+      return registry;
+    }
+
+    return createSafeFreezeRegistry(initialValues);
+  }
+
+  function hasSafeFreezeEntry(registry, value) {
+    if (!registry || !value) {
+      return false;
+    }
+
+    if (typeof registry.has === 'function') {
+      try {
+        return registry.has(value);
+      } catch (registryHasError) {
+        void registryHasError;
+        return false;
+      }
+    }
+
+    return hasArrayEntry(registry, value);
+  }
+
+  function assignToGlobal(namespace) {
+    var scope = detectAmbientScope();
+    if (!scope || _typeof(scope) !== 'object' && typeof scope !== 'function') {
+      return;
+    }
+
+    var registryName = 'cineCoreRuntimeStateModules';
+    var existing = scope[registryName] && _typeof(scope[registryName]) === 'object' ? scope[registryName] : {};
+    existing.safeFreezeRegistry = namespace;
+
+    try {
+      scope[registryName] = existing;
+    } catch (assignError) {
+      void assignError;
+    }
+  }
+
+  var namespace = {
+    createSafeFreezeRegistry: createSafeFreezeRegistry,
+    ensureSafeFreezeRegistry: ensureSafeFreezeRegistry,
+    hasSafeFreezeEntry: hasSafeFreezeEntry,
+    registerSafeFreezeEntry: registerSafeFreezeEntry
+  };
+
+  assignToGlobal(namespace);
+
+  if (typeof module === 'object' && module && module.exports) {
+    module.exports = namespace;
+  }
+})();
