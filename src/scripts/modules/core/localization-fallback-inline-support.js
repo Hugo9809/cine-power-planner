aaeccd9ddcd5ced7d5d90ec8194e8c0754c998d6
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-10-14 22:49:49 +0200

    Ensure runtime proxies track live exports

--- src/scripts/modules/core/localization-fallback-inline-support.js
+++ src/scripts/modules/core/localization-fallback-inline-support.js
@@ -1,4 +1,7 @@
 (function () {
+  var MODULE_ID = 'modules/core/localization-fallback-inline-support.js';
+  var REGISTRY_NAME = 'cineCoreLocalizationModules';
+
   function resolveLocalizationModules() {
     if (typeof require === 'function') {
       try {
@@ -8,59 +11,257 @@
       }
     }
 
-    var scope =
-      (typeof globalThis === 'object' && globalThis) ||
-      (typeof window === 'object' && window) ||
-      (typeof self === 'object' && self) ||
-      (typeof global === 'object' && global) ||
-      null;
+    var scope = getGlobalScope();
 
-    if (
-      scope &&
-      scope.cineCoreLocalizationModules &&
-      typeof scope.cineCoreLocalizationModules === 'object'
-    ) {
-      return scope.cineCoreLocalizationModules;
+    if (scope && scope[REGISTRY_NAME] && typeof scope[REGISTRY_NAME] === 'object') {
+      return scope[REGISTRY_NAME];
     }
 
     return null;
   }
 
-  var localizationModules = resolveLocalizationModules();
-  var namespace =
-    localizationModules &&
-    Object.prototype.hasOwnProperty.call(localizationModules, 'modules/core/localization-fallback-inline-support.js')
-      ? localizationModules['modules/core/localization-fallback-inline-support.js']
-      : null;
+  function getGlobalScope() {
+    if (typeof globalThis === 'object' && globalThis) {
+      return globalThis;
+    }
 
-  if (namespace == null) {
-    namespace = {};
+    if (typeof window === 'object' && window) {
+      return window;
+    }
+
+    if (typeof self === 'object' && self) {
+      return self;
+    }
+
+    if (typeof global === 'object' && global) {
+      return global;
+    }
+
+    return null;
   }
 
-  var globalScope =
-    (typeof globalThis === 'object' && globalThis) ||
-    (typeof window === 'object' && window) ||
-    (typeof self === 'object' && self) ||
-    (typeof global === 'object' && global) ||
-    null;
+  function isObjectLike(value) {
+    return !!value && (typeof value === 'object' || typeof value === 'function');
+  }
 
-  if (globalScope) {
-    var targetName = 'cineCoreLocalizationModules';
-    var existing =
-      globalScope[targetName] && typeof globalScope[targetName] === 'object'
-        ? globalScope[targetName]
-        : {};
+  function clearNamespace(target) {
+    if (!isObjectLike(target)) {
+      return;
+    }
 
-    existing['modules/core/localization-fallback-inline-support.js'] = namespace;
+    var names =
+      typeof Object.getOwnPropertyNames === 'function'
+        ? Object.getOwnPropertyNames(target)
+        : null;
 
-    try {
-      globalScope[targetName] = existing;
-    } catch (assignError) {
-      void assignError;
+    if (names) {
+      for (var index = 0; index < names.length; index += 1) {
+        var name = names[index];
+        try {
+          delete target[name];
+        } catch (deleteError) {
+          void deleteError;
+        }
+      }
+    } else {
+      for (var key in target) {
+        if (Object.prototype.hasOwnProperty.call(target, key)) {
+          try {
+            delete target[key];
+          } catch (fallbackDeleteError) {
+            void fallbackDeleteError;
+          }
+        }
+      }
+    }
+
+    if (typeof Object.getOwnPropertySymbols === 'function') {
+      var symbols = Object.getOwnPropertySymbols(target);
+      for (var symbolIndex = 0; symbolIndex < symbols.length; symbolIndex += 1) {
+        var symbol = symbols[symbolIndex];
+        try {
+          delete target[symbol];
+        } catch (deleteSymbolError) {
+          void deleteSymbolError;
+        }
+      }
     }
   }
 
-  if (typeof module === 'object' && module && module.exports) {
-    module.exports = namespace;
+  function copyNamespace(target, source) {
+    if (!isObjectLike(target) || !isObjectLike(source)) {
+      return target;
+    }
+
+    var define = typeof Object.defineProperty === 'function'
+      ? Object.defineProperty
+      : null;
+
+    var names =
+      typeof Object.getOwnPropertyNames === 'function'
+        ? Object.getOwnPropertyNames(source)
+        : null;
+
+    if (names) {
+      for (var index = 0; index < names.length; index += 1) {
+        var name = names[index];
+        var descriptor =
+          typeof Object.getOwnPropertyDescriptor === 'function'
+            ? Object.getOwnPropertyDescriptor(source, name)
+            : null;
+
+        if (descriptor && define) {
+          try {
+            define(target, name, descriptor);
+            continue;
+          } catch (defineError) {
+            void defineError;
+          }
+        }
+
+        target[name] = source[name];
+      }
+    } else {
+      for (var key in source) {
+        if (Object.prototype.hasOwnProperty.call(source, key)) {
+          target[key] = source[key];
+        }
+      }
+    }
+
+    if (typeof Object.getOwnPropertySymbols === 'function') {
+      var symbols = Object.getOwnPropertySymbols(source);
+      for (var symbolIndex = 0; symbolIndex < symbols.length; symbolIndex += 1) {
+        var symbol = symbols[symbolIndex];
+        var symbolDescriptor =
+          typeof Object.getOwnPropertyDescriptor === 'function'
+            ? Object.getOwnPropertyDescriptor(source, symbol)
+            : null;
+
+        if (symbolDescriptor && define) {
+          try {
+            define(target, symbol, symbolDescriptor);
+            continue;
+          } catch (defineSymbolError) {
+            void defineSymbolError;
+          }
+        }
+
+        target[symbol] = source[symbol];
+      }
+    }
+
+    return target;
   }
+
+  function synchronizeNamespace(target, source) {
+    if (target === source) {
+      return target;
+    }
+
+    if (!isObjectLike(target)) {
+      return isObjectLike(source) ? source : target;
+    }
+
+    if (!isObjectLike(source)) {
+      clearNamespace(target);
+      return target;
+    }
+
+    clearNamespace(target);
+    return copyNamespace(target, source);
+  }
+
+  var localizationModules = resolveLocalizationModules();
+  var namespace =
+    localizationModules &&
+    Object.prototype.hasOwnProperty.call(localizationModules, MODULE_ID)
+      ? localizationModules[MODULE_ID]
+      : null;
+
+  var globalScope = getGlobalScope();
+  var registry =
+    globalScope && globalScope[REGISTRY_NAME] && typeof globalScope[REGISTRY_NAME] === 'object'
+      ? globalScope[REGISTRY_NAME]
+      : null;
+
+  if (
+    !isObjectLike(namespace) &&
+    registry &&
+    Object.prototype.hasOwnProperty.call(registry, MODULE_ID)
+  ) {
+    var registryNamespace = registry[MODULE_ID];
+    if (isObjectLike(registryNamespace)) {
+      namespace = registryNamespace;
+    }
+  }
+
+  if (!isObjectLike(namespace)) {
+    namespace = {};
+  }
+
+  function updateExports(nextNamespace) {
+    namespace = nextNamespace;
+
+    if (typeof module === 'object' && module && module.exports) {
+      module.exports = nextNamespace;
+    }
+  }
+
+  if (!registry && globalScope) {
+    registry = {};
+  }
+
+  if (registry) {
+    if (!Object.prototype.hasOwnProperty.call(registry, MODULE_ID)) {
+      registry[MODULE_ID] = namespace;
+    } else {
+      var registryValue = registry[MODULE_ID];
+      if (isObjectLike(registryValue) && registryValue !== namespace) {
+        namespace = registryValue;
+      }
+    }
+
+    var namespaceRef = namespace;
+
+    function bridgeAssignment(value) {
+      if (value === namespaceRef) {
+        return;
+      }
+
+      if (isObjectLike(namespaceRef) && isObjectLike(value)) {
+        namespaceRef = synchronizeNamespace(namespaceRef, value);
+      } else {
+        namespaceRef = value;
+      }
+
+      updateExports(namespaceRef);
+    }
+
+    try {
+      Object.defineProperty(registry, MODULE_ID, {
+        configurable: true,
+        enumerable: true,
+        get: function () {
+          return namespaceRef;
+        },
+        set: function (value) {
+          bridgeAssignment(value);
+        },
+      });
+    } catch (defineError) {
+      void defineError;
+      registry[MODULE_ID] = namespaceRef;
+    }
+
+    if (globalScope && globalScope[REGISTRY_NAME] !== registry) {
+      try {
+        globalScope[REGISTRY_NAME] = registry;
+      } catch (assignError) {
+        void assignError;
+      }
+    }
+  }
+
+  updateExports(namespace);
 })();
