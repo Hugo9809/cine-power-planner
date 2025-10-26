6e1853aa572c4e1756aaf1112e696d42c2a0e785
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-10-26 20:06:43 +0100

    chore: bump version to 1.0.29

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function exposeAppVersion(globalScope) {
-  var version = '1.0.28';
+  var version = '1.0.29';
 
   function safelyAssign(target, key, value) {
     if (!target || (typeof target !== 'object' && typeof target !== 'function')) {
