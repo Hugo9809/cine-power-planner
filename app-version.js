ff9b3a51b5a30c477a95fcdf4a99214a35066f38
Hugo9809 <73077685+Hugo9809@users.noreply.github.com>
2025-12-24 01:43:56 +0100

    Bump version to 1.0.47

--- app-version.js
+++ app-version.js
@@ -1,5 +1,5 @@
 (function exposeAppVersion(globalScope) {
-  var version = '1.0.46';
+  var version = '1.0.47';
 
   function safelyAssign(target, key, value) {
     if (!target || (typeof target !== 'object' && typeof target !== 'function')) {
